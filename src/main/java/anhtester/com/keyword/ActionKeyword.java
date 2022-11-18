c9af005bcf327750de1039f3364ebc7610167add
Anh Tester <thaian.it15@gmail.com>
2022-11-18 10:58:45 +0700

    Update maven-surefire-plugin 3.0.0-M7

--- src/main/java/anhtester/com/keyword/ActionKeyword.java
+++ src/main/java/anhtester/com/keyword/ActionKeyword.java
@@ -4,16 +4,16 @@ import java.awt.*;
 import java.awt.event.KeyEvent;
 
 public class ActionKeyword {
-    public static void maximizeBrowserOnWindow(){
+    public static void maximizeBrowserOnWindow() {
         Robot rb = null;
         try {
             rb = new Robot();
+            rb.keyPress(KeyEvent.VK_WINDOWS);
+            rb.keyPress(KeyEvent.VK_UP);
+            rb.keyRelease(KeyEvent.VK_WINDOWS);
+            rb.keyRelease(KeyEvent.VK_UP);
         } catch (AWTException e) {
             e.printStackTrace();
         }
-        rb.keyPress(KeyEvent.VK_WINDOWS);
-        rb.keyPress(KeyEvent.VK_UP);
-        rb.keyRelease(KeyEvent.VK_WINDOWS);
-        rb.keyRelease(KeyEvent.VK_UP);
     }
 }
